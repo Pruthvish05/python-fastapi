a16f5e32b623ba73210d0d1bc7ae6a10a218ffcb
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-03-20 20:39:21 +0530

    tiny updates

--- tutorial_fastapi-03.py
+++ tutorial_fastapi-03.py
@@ -10,8 +10,9 @@ def get_students(student_id: int):
     if student_id in students:
         return students[student_id]
     return {"error": "Student not found"}
+#this shows not required query parameter
 @app.get("/get-student")
-def get_student(name : str):
+def get_student(name : str = None):
     for student in students.values():
         if student["name"] == name:
             return student
