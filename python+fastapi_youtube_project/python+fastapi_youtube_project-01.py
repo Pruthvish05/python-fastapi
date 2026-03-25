ad7f819d69b0f256d03f626a3a2aaff84ed4e358
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-03-25 21:18:22 +0530

    Update python+fastapi_youtube_project-01.py

--- python+fastapi_youtube_project/python+fastapi_youtube_project-01.py
+++ python+fastapi_youtube_project/python+fastapi_youtube_project-01.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
 app = FastAPI()
-@app.post("/")
-def create_item(item: dict):
+books = [
+    {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
+    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    {"id": 3, "title": "1984", "author": "George Orwell"},
+]
+@app.get("/books")
+def read_books():
+    return {"books": books}
