8bc370b720ea8f60371f405233c99b947e31cf28
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-04-01 00:06:00 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,20 @@
 from collections.abc import AsyncGenerator
-from sqlalchemy import text, create_engine, column, String,DateTime,ForeignKey
+from sqlalchemy import Text, create_engine, column, String,DateTime,ForeignKey
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
-DATABASE_URL = "sqlite_+aiosqlite:///./test.db"
+import datetime
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+class Posts(DeclarativeBase):
+    __tablename__ = "posts"
+    id = column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    caption = column(Text)
+    url = column(String, nullable=False)
+    created_at = column(DateTime, default=datetime.utcnow)
+    file_name = column(String, nullable=False)
+    file_type = column(String, nullable=False)
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
