f2340744f8e8593e6dc096ed385cd4f6ffe146c0
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-04-03 01:18:30 +0530

    cooked

--- smart_notes_tracker/main_1.py
+++ smart_notes_tracker/main_1.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
+
+from requests import Session
 #we add somethings ukwim.
 from database import SessionLocal, engine, Base
 import models
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
-Notes = []
-class Note(BaseModel):
+# Notes = []#done with this temporary storagefor notes
+class NoteCreate(BaseModel):
     title: str
     content: str
 
@@ -27,28 +29,30 @@ def root():
 
 
 @app.post("/notes")
-def create_note(note: Note):
-    note_dict = note.dict()
-    note_dict["id"] = len(Notes) + 1
-    Notes.append(note_dict)
-    return {"message": "Note created successfully", "note": note}
+def create_note(note: NoteCreate, db:Session = Depends(get_db)):
+    db_note = models.Note(title=note.title, content=note.content)
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return {"message": "Note created successfully", "note": db_note}
+
 
 @app.get("/notes")
-def get_notes():
-    return {"notes": Notes}
+def get_notes(db: Session = Depends(get_db)):
+    return {"notes": db.query(models.Note).all()}
 
 @app.get("/notes/{note_id}")
-def get_note(note_id: int):
-    for note in Notes:
-        if note["id"] == note_id:
-            return {"note": note}
-    raise HTTPException(status_code=404, detail="Note not found")          
+def get_note(note_id: int,db: Session = Depends(get_db)):
+    note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return {"note": note}
 
 @app.delete("/notes/{note_id}")
-def delete_note(note_id: int):
-    for note in Notes:
-        if note["id"] == note_id:
-            Notes.remove(note)
-            len(Notes) - 1
-            return {"message": "Note deleted successfully"}
-    raise HTTPException(status_code=404, detail="Note not found")
+def delete_note(note_id: int, db: Session = Depends(get_db)):
+    note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if note is None:
+        raise HTTPException(status_code=404, detail="Note not found")
+    db.delete(note)
+    db.commit()
+    return {"message": "Note deleted successfully"}
