d6bf21d6b0b387e8fbf33190eb3455df919d88c6
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-04-02 23:32:43 +0530

    updates

--- python+fastapi_youtube_project/db.py
+++ python+fastapi_youtube_project/db.py
@@ -21,3 +21,6 @@ async_session = async_sessionmaker(engine, expire_on_commit=False)
 async def create_db_with_tables() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(DeclarativeBase.metadata.create_all)
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
